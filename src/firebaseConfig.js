e9b3024141467cf2995b835f8f271cbe8285f3ec
AlejandraDay <alejandra.day.123@gmail.com>
2020-11-26 23:11:38 -0400

    firebase init.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// firebase init
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWGmx-WuPNpEKh1GiuyapbZ2aSF-AsZkk",
+  authDomain: "infosalud-4795a.firebaseapp.com",
+  databaseURL: "https://infosalud-4795a.firebaseio.com",
+  projectId: "infosalud-4795a",
+  storageBucket: "infosalud-4795a.appspot.com",
+  messagingSenderId: "106181316869",
+  appId: "1:106181316869:web:6d20d2bcd7d4434614276c",
+  measurementId: "G-PR8P0HPPVX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// firebase utils
+const db = firebase.firestore();
+
+// firebase collectionsa
+//const hospitalCollection = db.collection("hospital");
+//const orderCollection = db.collection("buyout_order");
+export { firebase, db};
